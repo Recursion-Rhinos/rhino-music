23e1f220b37efd0b735c1cf83f090c9ed09a1ed3
Svetlin Mladenov <svem@gmx.com>
2016-12-18 00:17:44 -0500

    Users table

--- server/db/db.js
+++ server/db/db.js
@@ -4,6 +4,25 @@ const knex = require('knex')({
     host: '127.0.0.1',
     user: '<User Name>',
     password: '<Your password>',
+    database: '<DB name>',
     charset: 'utf8'
+  },
+  useNullasDefault: true
+});
+
+knex.schema.hasTable('Users').then(function(exist) {
+  if (!exist) {
+    knex.schema.createTable('Users', function(user) {
+      user.increments('id').primary();
+      user.string('username');
+      user.string('password'); //charset
+      user.string('email');
+    }).then(function(table) {
+      console.log('Created Table', table);
+    });
   }
 });
+
+
+
+
