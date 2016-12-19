c788aaa4d4df8fcef82e7491a734b290086ba43e
Svetlin Mladenov <svem@gmx.com>
2016-12-19 12:14:26 -0500

    created store function in user model

--- server/messages/messagesModel.js
+++ server/messages/messagesModel.js
@@ -1,2 +1,6 @@
 var knex = require('../database/schema.knex.js');
+
+
+
+
 module.exports = {};
--- server/user/userModel.js
+++ server/user/userModel.js
@@ -1,2 +1,21 @@
 const knex = require('../database/schema.knex.js');
-module.exports = {};
+//1 Function for saving username, password, email and genres
+//1 Function for get all data back
+let store = (username, password, email, genres) => {
+  return knex('Users').insert({ 
+    username: username, 
+    password: password,
+    email: email,
+    genres: genres
+  });
+};
+
+let getAll = () => {
+  
+};
+
+
+module.exports = {
+  store: store,	
+  getAll: getAll
+};
