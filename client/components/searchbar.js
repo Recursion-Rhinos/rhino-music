1028ff5f3f191860a42482bc4b1bce4011647d00
nanarmeen12 <narminshahin@gmail.com>
2016-12-19 16:16:26 -0500

    editing search bar

--- client/components/searchbar.js
+++ client/components/searchbar.js
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
-const SearchBar = () => {
-	
+class SearchBar extends Component {
+
+	constructor(props) {
+		super(props);
+		
+		this.state = { term: '' }
+	}
+
+render() {
+
+	return (
+
+	<div>
+	 	<input 
+	 	value={this.state.term}   {//this.setState causes component to rerender and when it rerenders its set to this.state.term
+	 								}
+	 	onChange={event =>  this.setState({term: event.target.value})}/>
+
+	</div>
+
+		)
+	}
 }
+
+
+export default SearchBar;
