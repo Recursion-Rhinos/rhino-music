46b81dc8eae5c6a8a186fb90249369d68a1b4e37
Svetlin Mladenov <svem@gmx.com>
2016-12-21 10:49:45 -0500

    modified SongItem Component

--- client/components/search_results.js
+++ client/components/search_results.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import SearchBar from './searchbar';
+// import SearchBar from './searchbar';
+import SongItem from './song_item';
 
 const SearchResults = (props) => {
   console.log("SearchResults", props);
--- client/components/song_item.js
+++ client/components/song_item.js
@@ -1,4 +1,21 @@
 import React from 'react';
+// import SearchResult from './search_results';
 
+const SongItem = (props) => {
+  console.log(props);
+  return (
+    <li className="list-group-item">
+      <div className="song-list-media">
+        <div className="song-left">     
+          <img className="song-object" src={songImg} />
+        </div>
+
+        <div className="song-body">
+          <div className="song-heading">{window.spotify.name}</div>
+        </div>
+      </div>
+    </li>
+  );
+};
 
 export default SongItem;
