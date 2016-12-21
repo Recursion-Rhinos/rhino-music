e24e55e10de30e14e2c1a5ebdfa123fd05d42845
Svetlin Mladenov <svem@gmx.com>
2016-12-21 16:00:10 -0500

    songItem Controller

--- client/components/search_results.js
+++ client/components/search_results.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import SearchBar from './searchbar';
-import SongItem from './song_item';
+// import SongItem from './song_item';
 
 const SearchResults = (props) => {
   console.log("SearchResults", props);
--- client/components/song_item.js
+++ client/components/song_item.js
@@ -7,11 +7,11 @@ const SongItem = (props) => {
     <li className="list-group-item">
       <div className="song-list-media">
         <div className="song-left">     
-          <img className="song-object" src={songImg} />
+          <img className="song-object" />
         </div>
 
         <div className="song-body">
-          <div className="song-heading">{window.spotify.name}</div>
+          <div className="song-heading">Some props</div>
         </div>
       </div>
     </li>
