fbdb8a8bc8a1f5b84326a2a6a8b1f937204f0b74
Svetlin Mladenov <svem@gmx.com>
2016-12-21 19:30:46 -0500

    rendered songs

--- client/components/song_item.js
+++ client/components/song_item.js
@@ -1,17 +1,17 @@
 import React from 'react';
 // import SearchResult from './search_results';
 
-const SongItem = (props) => {
+const SongItem = (props, onSongSelect) => {
   console.log("SongItem",props);
   return (
-    <li onClick = {() => onSongSelect(props.songs.uri)} className="list-group-item">
+    <li onClick = {() => onSongSelect(props.song.uri)} className="list-group-item">
       <div className="song-list-media">
         <div className="song-left">     
           <img className="song-object" />
         </div>
       
         <div className="song-body">
-          <div className="song-heading">Some props</div>
+          <div className="song-heading">{props.song.name}</div>
         </div>
       </div>
     </li>
