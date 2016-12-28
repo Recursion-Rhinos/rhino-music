024019eb63d59b3eb733a63f0fd40d3ef3021c9e
Svetlin Mladenov <svem@gmx.com>
2016-12-28 18:12:08 -0500

    Add NYTimes reducer

---
+++ client/reducers/news_search.js
@@ -0,0 +1,10 @@
+import { NYT_NEWS } from '../constants/ActionTypes';
+
+export default function(state={}, action) {
+  console.log('REDUCER-NEWS', action);
+  switch(action.type) {
+    case NYT_NEWS:
+    return state.concat([ action.payload.data ]);
+  }  
+  return state;
+}
