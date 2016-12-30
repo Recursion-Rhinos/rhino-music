bb8f7c51caac53664859cf66fcf44afe561c8df1
Svetlin Mladenov <svem@gmx.com>
2016-12-30 12:21:57 -0500

    added reducer_songs

--- client/actions/songs.js
+++ client/actions/songs.js
@@ -2,7 +2,7 @@ import { PLAY_SONGS } from '../constants/ActionTypes';
 
 export function playSong(songId) {
   return {
-    type: PLAY_SONGS,
+    type: "PLAY_SONGS",
     payload: songId	
   };
 }
