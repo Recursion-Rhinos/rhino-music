55856f8f169f2496298e4f2481db030bd8cbad41
Svetlin Mladenov <svem@gmx.com>
2017-01-01 16:41:48 -0500

    fixed action creator for nytimes

--- client/actions/news_nytimes.js
+++ client/actions/news_nytimes.js
@@ -1,35 +1,53 @@
 import axios from 'axios';
-import * as types from '../constants/ActionType';
-// const NEWS_URL = "http://localhost:3005/news";
+import { NYT_NEWS } from '../constants/ActionType';
+// const NEWS_URL = "http://localhost:3005/news"
+
+const newsData = (news) => {
+  console.log("news", news)	
+  return {
+    type: NYT_NEWS,
+    news: news
+  }
+};
 
 
 export function fetchNews() {
-  console.log("inside fetchNews get request");
-  return function (dispatch) {
-    const news = "http://localhost:3005/api/news";
-      axios.get(news, {})
-      .then(res => {
-        console.log("inside axios-get", res);
-        dispatch({
-          type: types.NYT_NEWS,
-          news: res
-        })
-        .catch(error => console.log('Error in NYTimes API', error));
-      });
+  return (dispatch) => {
+    return axios.get('/api/news')
+      .then((response) => {
+      	console.log("RESPONSE", response)
+        dispatch(newsData(response));
+      })
+      .catch((error) => console.log(error));
   }
 }
-  .then(data => {
-   console.log("NEWS DATA", data);
-   })
-  .catch(error => {
-    console.log(error);
-  })
+// export function fetchNews() {
+//   console.log("inside fetchNews get request");
+//   return function (dispatch) {
+//     const news = "http://localhost:3005/api/news";
+//       axios.get(news, {})
+//       .then(res => {
+//         console.log("inside axios-get", res);
+//         dispatch({
+//           type: types.NYT_NEWS,
+//           news: res
+//         })
+//         .catch(error => console.log('Error in NYTimes API', error));
+//       });
+//   }
+// }
+//   .then(data => {
+//    console.log("NEWS DATA", data);
+//    })
+//   .catch(error => {
+//     console.log(error);
+//   })
  
-  console.log("NEWS", newsdata);
+//   console.log("NEWS", newsdata);
 
-  return {
-    type: types.NYT_NEWS,
-    payload: newsdata	
-  };
-}
+//   return {
+//     type: types.NYT_NEWS,
+//     payload: newsdata	
+//   };
+// }
 
--- client/reducers/news_search.js
+++ client/reducers/news_search.js
@@ -3,7 +3,7 @@ import { NYT_NEWS }  from '../constants/ActionTypes';
 export default function (state=[], action) {
   console.log('REDUCER-NEWS', action);
   switch (action.type) {
-    case NYT_NEWS:
+    case "NYT_NEWS":
     return state.concat([ action.payload.data ]);
   }
   return state;
