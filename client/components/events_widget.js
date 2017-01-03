aa77766a8634f7597f57a0e053803b87b918b62b
nanarmeen12 <narminshahin@gmail.com>
2017-01-03 14:41:05 -0500

    minor youtube changes. got the props through to the render list function in the video playlist component

--- client/components/events_widget.js
+++
@@ -1,3 +0,0 @@
-
-   <script type='text/javascript' src='http://www.bandsintown.com/javascripts/bit_widget.js'></script>
-  <a href='http://www.bandsintown.com' id="widgetInput" class='bit-widget-initializer' data-artist=''>Bandsintown</a>
--- client/containers/youtube_playlist.js
+++ client/containers/youtube_playlist.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { fetchVideos } from '../actions/youtube';
+import { playVideo } from '../actions/videos';
 
 
 class YoutubePlaylist extends Component {
@@ -11,17 +11,16 @@ class YoutubePlaylist extends Component {
 		this.renderList = this.renderList.bind(this);
 	}
 
-   renderList() {
+   renderList(vids) {
 
-
-
+   	console.log("RENDER LIST VIDEOSSS", vids)
 
 
 
    return (
 
    		<li>
-
+   			tktk
     	</li>
 
     	)
@@ -30,17 +29,18 @@ class YoutubePlaylist extends Component {
 
 render () {
   return (
-
+  	<div>
+  	<h1> Video Playlist </h1>
 	    <ul className="list-group col-sm-4">
-	    	{this.renderList()}
+	    	{this.props.videos.map(this.renderList)}
 	    </ul>
-
+	</div>
 	  );
   }
 }
 
 function mapStateToProps(state) {
-  return {tracks: state.tracks};  //same as tracks: state.tracks
+  return {videos: state.videos};  //same as tracks: state.tracks
 }
 
 function mapDispatchToProps(dispatch) {
