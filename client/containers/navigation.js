fe2d4fd5f03fb9921c9a997b9be06e3ebc503ec4
Svetlin Mladenov <svem@gmx.com>
2017-01-03 14:20:55 -0500

    modified navigation and nytimes.js

--- client/containers/navigation.js
+++ client/containers/navigation.js
@@ -5,20 +5,24 @@ import { playSong } from '../actions/songs';
 import { fetchNews } from '../actions/news_nytimes';
 import { fetchEvents } from '../actions/events';
 import { bindActionCreators } from 'redux';
+import nyTimesData  from '../containers/nytimes_data';
 
 class Navigation extends Component {
   constructor(props) {
     super(props);
+
+    // this.state = {term: ''};
     console.log("NAVIGATION", this.props)
   }
 
  render() {
  	console.log("NAVIGATION2", this.props)
+  console.log("Container", nyTimesData)
    return (
      <div>
        <button type = "button" class = "btn btn-default">Events</button>	
        <button type = "button" class = "btn btn-default">Videos</button>
-       <button onClick={() => console.log("NEWS NEWS NEWS")} type = "button" class = "btn btn-default">News</button>	
+       <button onClick={() => )} type = "button" class = "btn btn-default">News</button>	
        <button type = "button" class = "btn btn-default">Profile</button>
        <button type = "button" class = "btn btn-default">Logout</button>
      </div>
