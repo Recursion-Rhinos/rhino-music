4f5e93cfe63ba86f4164e6fbc77a2179b9a06d1d
nanarmeen12 <narminshahin@gmail.com>
2017-01-03 19:14:33 -0500

    fixed list items rendering in one li

--- client/containers/youtube_playlist.js
+++ client/containers/youtube_playlist.js
@@ -11,27 +11,48 @@ class YoutubePlaylist extends Component {
 		this.renderList = this.renderList.bind(this);
 	}
 
-   renderList(vids) {
+   renderList() {
+   	
+   	console.log('THIS DA VID PROP. DUDE: ', this.props.videos);
+   	let videosArray = [];
+   	if(this.props.videos.length > 0) {
+   		videosArray = this.props.videos[0];
+   	}
 
-  	return this.props.vids.map((videos) => {
-  		return (
-  			 	<li>
-      {videoThumbnail}
+   	// let videoId = vids.map((video) => video.id.videoId)
+
+   	// const videoThumbnail = vids
+   	// .map((image) => image.snippet.thumbnails.default.url)
+   	// .map((thumbnail, idx) => (<img key={videoId.concat(idx + 1)} src={thumbnail} />));
+
+   	// const title = vids
+   	// .map((name) => name.snippet.title)
+   	// .map((titles) => titles)
+   	console.log('THIS DA VIDEOZZZZZZZ: ', videosArray);
+
+   return (
+	videosArray.map((video) => {
+	return (
+   	<li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={video.snippet.thumbnails.default.url} />
+        </div>
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
     </li>
-  		)
-  	})
-
-   	const videoThumbnail = vids
-   	.map((image) => image.snippet.thumbnails.default.url)
-   	.map((thumbnail, idx) => (<img key={idx} src={thumbnail} />));
-
+		)
+	})
+    	)
   
   }
 
 render () {
   return (
 	    <ul className="list-group col-sm-4">
-	    	{this.renderList(this.props.videos}
+	    	{this.renderList()}
 	    </ul>
 
 	  );
--- client/reducers/youtube_search.js
+++ client/reducers/youtube_search.js
@@ -4,7 +4,6 @@ export default function(state = [], action) {
 
   switch (action.type) {
   case "FETCH_VIDEOS":
-  console.log("VIDEOSOOSOOSOSSSSS", action.payload.data.items)
  	return [action.payload.data.items,...state];
   }
   return state;
