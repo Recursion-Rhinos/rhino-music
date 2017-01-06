6e7195b7903287e5b7cde43291383f9e10868b44
Svetlin Mladenov <svem@gmx.com>
2017-01-06 11:27:34 -0500

    added React Router

--- client/index.js
+++ client/index.js
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
-import Thunk from 'redux-thunk'
-
-import App from './components/app';
+import Thunk from 'redux-thunk';
+import { Router, browserHistory } from 'react-router'; //browserHistory, hashHistory and memoryHistory
 import reducers from './reducers';
+import routes from './routes';
+// import App from './components/app';
+
 
 const createStoreWithMiddleware = applyMiddleware(Thunk, ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory} routes={routes}/>
   </Provider>
   , document.querySelector('.container'));
--- client/routes.js
+++ client/routes.js
@@ -1,9 +1,11 @@
-// import React from 'react';
-// import { Router, Route } from 'react-router';
+import React from 'react';
+import { Router, IndexRoute } from 'react-router';
 
-// import App from './components/index.js';
+import App from './components/app.js';
 
-
+export default (
+  <Router path="/search" component={App} /> // /search?
+)
 // const Routes = (props) => (
 //   <Router {...props}>
 //     <Route path="/api/search" component={App} />
