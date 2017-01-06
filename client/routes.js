4e1472ba8f4c8dd870bb0d52f197b8cc0dee9aa0
Svetlin Mladenov <svem@gmx.com>
2017-01-06 15:44:26 -0500

    added routes

--- client/routes.js
+++ client/routes.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import { Router, IndexRoute } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 
-import App from './components/app.js';
+import App from './components/app';
+import Navigation from './containers/navigation'
+import SearchBar from './containers/search_bar';
+import SearchResults from './containers/search_results_list';
+import MusicPlayer from './containers/spotify_player';
+import NYTimes from './containers/nytimes';
+import VideoPlaylist from './containers/youtube_playlist';
+import UserPlaylists from './containers/myMusic.js';
+import VideoPlayer from './containers/youtube_player';
+import EventsList from './containers/events_list.js';
 
 export default (
-  <Router path="/search" component={App} /> // /search?
-)
-// const Routes = (props) => (
-//   <Router {...props}>
-//     <Route path="/api/search" component={App} />
-//     // <Route path="/about" component={About} />
-//     // <Route path="*" component={NotFound} />
-//   </Router>
-// );
+  <Route path="/search" component={App} >
+    <Route path="/search" component={ Navigation } />
+    <Route path="/search" component={SearchBar} />
+    <Route path="/search" component={MusicPlayer} />
+    <Route path="/search" component={NYTimes} />
+    <Route path="/search" component={VideoPlayer} />
+    <Route path="/search" component={VideoPlaylist} />
+    <Route path="/search" component={UserPlaylists} />
+    <Route path="/search" component={EventsList} />
+  </Route>
+);
 
 // export default Routes;
