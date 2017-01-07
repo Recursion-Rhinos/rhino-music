87a6107131be3bc73a34ea68350ab06ad0b98a5f
Jerry Shanahan <jerry.shanahan@gmail.com>
2017-01-07 16:41:16 -0500

    trying to render playlistSongs onClick of playlist

--- client/containers/myMusic.js
+++ client/containers/myMusic.js
@@ -9,7 +9,21 @@ console.log("GET PLAYLIST SONGS", getPlaylistSongs)
 class UserPlaylists extends Component {
   constructor(props) {
     super(props);
+    this.state = null;
+    this.renderSongs = this.renderSongs.bind(this);
     this.renderList = this.renderList.bind(this);
+  }
+
+  renderSongs(songs) {
+    console.log('SONGS!!!!!!!!!', songs)
+    // if(songs === null) {
+    //   return <tr><td></td></tr>;
+    // } else {
+    //   return songs.map((song) => {
+    //     console.log('RENDER THIS BITCH: ', song)
+    //     return
+    //   })
+    // }
   }
 
   renderList(playlists) {
@@ -17,7 +31,7 @@ class UserPlaylists extends Component {
     return playlists.map((playlist) => {
       //console.log("myMusic.js playlist: ", playlist);
       return (
-        <tr key={playlist.id} onClick={() => renderPlaylistSongs(this.props.getPlaylistSongs(playlist.Name))}>
+        <tr key={playlist.id} onClick={() => this.renderSongs(this.props.getPlaylistSongs(playlist.Name))}>
           <td>{playlist.Name}</td>
         </tr>
         // <TableRow key={playlist.id} onClick={() => this.props.getPlaylistSongs(playlist.Name)}>
@@ -31,9 +45,15 @@ class UserPlaylists extends Component {
   render() {
     console.log('USER PLAYLIST PROPS: ', this.props)
     return (
-      <table>
-       {this.renderList(this.props.playlists)}
-      </table>
+      <div>
+        <table>
+         {this.renderList(this.props.playlists)}
+        </table>
+      </div>
+        // <table>
+        // {this.renderSongs(this.state)}
+        // </table>
+      
        // <Table>
       // <TableBody>
        // { console.log("myMusic.js => this.props", this.props)}
