dcb19e0e5e598441a824a664595886943ca5167a
Svetlin Mladenov <svem@gmx.com>
2017-01-10 22:24:27 -0500

    Added experimental Material-UI Drawer

--- client/containers/Profile/userMenu.js
+++ client/containers/Profile/userMenu.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export default class Profile extends Component {
   constructor(props) {
     super(props);
+     this.state = {open: false};
   }	
+
+   handleToggle = () => this.setState({open: !this.state.open});
+
 
 //Facebook image:
 //picture: profile.photos ? profile.photos[0].value : '/img/faces/unknown-user-pic.jpg'
@@ -13,12 +20,21 @@ export default class Profile extends Component {
 render () { 
     return (
       <div>
-       <p>Videos</p>
-       <p>Events</p>
-       <p>News</p>
-       <p>Logout</p>
-      {console.log("PROPS", this.props)}
+      <RaisedButton
+        label="Toggle Drawer"
+        onTouchTap={this.handleToggle}
+      />
+      <Drawer open={this.state.open}>
+        <MenuItem>Home</MenuItem>
+        <MenuItem>Playlists</MenuItem>
+        <MenuItem>Events</MenuItem>
+        <MenuItem>Videos</MenuItem>
+        <MenuItem>News</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Drawer>
       </div>
     );
   }
 }
+
+
