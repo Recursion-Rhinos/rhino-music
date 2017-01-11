8711f82013f8d376aec0cf22c8b6ba621797ee29
Svetlin Mladenov <svem@gmx.com>
2017-01-11 11:38:27 -0500

    Completed Material-UI table for Events

--- client/containers/Profile/userFavourites.js
+++ client/containers/Profile/userFavourites.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Table, TableBody, TableFooter, TableHeader, TableHeaderColumn, TableRow, TableRowColumn }from 'material-ui/Table';
+import TextField from 'material-ui/TextField';
+import Toggle from 'material-ui/Toggle';
 import getAllPlaylists  from '../../actions/getPlaylists';
 import getEvents  from '../../actions/profileEvents';
 import removeEvent from '../../actions/removeEvent';
+
+
+const styles = {
+  propContainer: {
+    width: 200,
+    overflow: 'hidden',
+    margin: '20px auto 0',
+  },
+  propToggleHeader: {
+    margin: '20px auto 10px',
+  }
+};
+
 
 class Favourites extends Component {
   constructor(props) {
     super(props)
 
+     this.state = {
+      fixedHeader: true,
+      fixedFooter: true,
+      stripedRows: false,
+      showRowHover: true,
+      selectable: false,
+      multiSelectable: false,
+      enableSelectAll: false,
+      deselectOnClickaway: true,
+      showCheckboxes: false,
+      height: '300px',
+    };
     this.componentWillMount = this.componentWillMount.bind(this);
     // this.test = this.props.getEvents()
     // this.getUserEvents = this.getUserEvents.bind(this);
@@ -22,6 +50,10 @@ componentWillMount() {
     console.log("getEvents return a promise: => ", this.props.allEvents)
 }
 
+componentDidReceiveProps(nextProps) {
+  this.forceUpdate();
+};
+
 handleToggle(event, toggled) {
   this.setState({
   [event.target.name]: toggled,
@@ -32,10 +64,6 @@ handleChange (event) {
   this.setState({height: event.target.value});
 };
 
-componentDidReceiveProps(nextProps) {
-  this.forceUpdate();
-};
- 
   renderPlaylists(playlists) {
     console.log("PLAYLISTS INSINDE RENDERPLAYLISTS", playlists);
     if(Array.isArray(playlists)) {
@@ -69,11 +97,12 @@ componentDidReceiveProps(nextProps) {
     return data.map((el, idx) =>{
       console.log("+++++++++++++++++++++++++++", el)
       return (
-        <tr key={el + (idx + Math.random())}>
-          <td>{el.event.name}</td> 
-          <td>{el.event.location[0]}</td>
-          <td><button onClick={() => {this.props.removeEvent(el.id); this.props.getEvents()}}>Remove</button></td>   
-        </tr>
+      <TableRow  key={el + (idx + Math.random())} >
+        <TableRowColumn >{ idx }</TableRowColumn>
+        <TableRowColumn>{el.event.name}</TableRowColumn>
+        <TableRowColumn>{el.event.location[0]}</TableRowColumn>
+        <TableRowColumn><button onClick={() => {this.props.removeEvent(el.id); this.props.getEvents()}}>Remove</button></TableRowColumn> 
+      </TableRow>
       )  
     });
   }
@@ -81,24 +110,49 @@ componentDidReceiveProps(nextProps) {
   render () {
     console.log("SVETDaVeT Playlist", this.props)
    return (
+    // <div>
+    //   <div>
+    //    <table>
+    //    <tr>
+    //     <th>Playlists</th> 
+    //       <tr>{this.renderPlaylists(this.props.playlists)}</tr>
+    //    </tr>
+    //    </table> 
+    //   </div>
     <div>
-      <div>
-       <table>
-       <tr>
-        <th>Playlists</th> 
-          <tr>{this.renderPlaylists(this.props.playlists)}</tr>
-       </tr>
-       </table> 
-      </div>
-      <div> 
-       <table>
-         <tr>
-         <th>Events</th>
-           <tr>{this.renderEvents(this.props.allEvents)}</tr>
-         </tr>
-       </table>
-      </div>  
-      </div>  
+    <Table
+      height={this.state.height}
+      fixedHeader={this.state.fixedHeader}
+      fixedFooter={this.state.fixedFooter}
+      selectable={this.state.selectable}
+      multiSelectable={this.state.multiSelectable}
+    >
+    <TableHeader
+      displaySelectAll={this.state.showCheckboxes}
+      adjustForCheckbox={this.state.showCheckboxes}
+      enableSelectAll={this.state.enableSelectAll}
+    >
+    <TableRow>
+    <TableHeaderColumn colSpan="3" tooltip="EVENTS" style={{textAlign: 'center'}}>
+      EVENTS
+    </TableHeaderColumn>
+    </TableRow>
+    <TableRow>
+      <TableHeaderColumn tooltip="The ID">ID</TableHeaderColumn>
+      <TableHeaderColumn tooltip="The Name">Name</TableHeaderColumn>
+      <TableHeaderColumn tooltip="The Location">Location</TableHeaderColumn>
+    </TableRow>
+    </TableHeader>
+    <TableBody
+      showRowHover={this.state.showRowHover}
+      displayRowCheckbox={this.state.showCheckboxes}
+      deselectOnClickaway={this.state.deselectOnClickaway}
+      stripedRows={this.state.stripedRows}
+    >
+      {this.renderEvents(this.props.allEvents)}
+    </TableBody> 
+    </Table>   
+    </div>  
     );
   }
 }
