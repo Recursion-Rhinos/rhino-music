a9f67417f30a5c13d8769333e422660ec935a2cf
Svetlin Mladenov <svem@gmx.com>
2017-01-11 14:00:55 -0500

    Hide/show  User settings in progress

--- client/containers/Profile/userMenu.js
+++ client/containers/Profile/userMenu.js
@@ -12,7 +12,7 @@ import ContentCopy from 'material-ui/svg-icons/content/content-copy';
 import Download from 'material-ui/svg-icons/file/file-download';
 import Delete from 'material-ui/svg-icons/action/delete';
 import FontIcon from 'material-ui/FontIcon';
-
+import Setting from './userSettings';
 
 const style = {
   paper: {
@@ -25,8 +25,20 @@ const style = {
 export default class Profile extends Component {
   constructor(props) {
     super(props);
+    // console.log("=============>", Setting)
+    // console.log("PROPS+++++++++++++++", this.props)
      this.state = {open: false};
+     this.onClick = this.onClick.bind(this);
+     this.getInitialState = this.getInitialState.bind(this);
   }	
+
+  getInitialState() {
+    return { childVisible: false };
+  }
+
+  onClick() {
+    this.setState({childVisible: ! this.state.childVisible});
+  }
 
 //Facebook image:
 //picture: profile.photos ? profile.photos[0].value : '/img/faces/unknown-user-pic.jpg'
@@ -37,7 +49,8 @@ render () {
       <Paper style={style.paper}>
       <Menu>
         <MenuItem primaryText="Svetlin" leftIcon={<RemoveRedEye />} />
-        <MenuItem primaryText="Basic Details" onClick={() => console.log("TEST")} leftIcon={<PersonAdd />} />
+        <MenuItem primaryText="Basic Details" onClick={this.onClick} leftIcon={<PersonAdd />} />
+        { this.state.childVisible ? <Setting />: null }
         <MenuItem primaryText="Playlists" leftIcon={<ContentLink />} />
         <Divider />
         <MenuItem primaryText="Events" leftIcon={<ContentCopy />} />
