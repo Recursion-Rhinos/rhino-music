8590be6caac9fab8b5ac1521e32a99c40eae176d
Svetlin Mladenov <svem@gmx.com>
2017-01-11 13:45:56 -0500

    added menu

--- client/actions/getPlaylists.js
+++ client/actions/getPlaylists.js
@@ -3,7 +3,7 @@ import { GET_ALL_PLAYLISTS } from '../constants/ActionTypes';
 
 export function getAllPlaylists() {
 
-  let request = axios.get('/api/myMusic');
+  let request = axios.get('/api/profilePlaylists');
   console.log("getPlayList in Action creator", request)
   return {
     type: GET_ALL_PLAYLISTS,
--- client/containers/Profile/userMenu.js
+++ client/containers/Profile/userMenu.js
@@ -1,9 +1,26 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import Drawer from 'material-ui/Drawer';
+import Paper from 'material-ui/Paper';
+import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
-import RaisedButton from 'material-ui/RaisedButton';
+import RemoveRedEye from 'material-ui/svg-icons/image/remove-red-eye';
+import PersonAdd from 'material-ui/svg-icons/social/person-add';
+import ContentLink from 'material-ui/svg-icons/content/link';
+import Divider from 'material-ui/Divider';
+import ContentCopy from 'material-ui/svg-icons/content/content-copy';
+import Download from 'material-ui/svg-icons/file/file-download';
+import Delete from 'material-ui/svg-icons/action/delete';
+import FontIcon from 'material-ui/FontIcon';
+
+
+const style = {
+  paper: {
+    display: 'inline-block',
+    float: 'left',
+    margin: '16px 32px 16px 0',
+  }
+};
 
 export default class Profile extends Component {
   constructor(props) {
@@ -11,27 +28,22 @@ export default class Profile extends Component {
      this.state = {open: false};
   }	
 
-   handleToggle = () => this.setState({open: !this.state.open});
-
-
 //Facebook image:
 //picture: profile.photos ? profile.photos[0].value : '/img/faces/unknown-user-pic.jpg'
 
 render () { 
     return (
       <div>
-      <RaisedButton
-        label="Toggle Drawer"
-        onTouchTap={this.handleToggle}
-      />
-      <Drawer open={this.state.open}>
-        <MenuItem>Home</MenuItem>
-        <MenuItem>Playlists</MenuItem>
-        <MenuItem>Events</MenuItem>
-        <MenuItem>Videos</MenuItem>
-        <MenuItem>News</MenuItem>
-        <MenuItem>Logout</MenuItem>
-      </Drawer>
+      <Paper style={style.paper}>
+      <Menu>
+        <MenuItem primaryText="Svetlin" leftIcon={<RemoveRedEye />} />
+        <MenuItem primaryText="Basic Details" onClick={() => console.log("TEST")} leftIcon={<PersonAdd />} />
+        <MenuItem primaryText="Playlists" leftIcon={<ContentLink />} />
+        <Divider />
+        <MenuItem primaryText="Events" leftIcon={<ContentCopy />} />
+        <MenuItem primaryText="Social " leftIcon={<Download />} />
+      </Menu>
+      </Paper>
       </div>
     );
   }
