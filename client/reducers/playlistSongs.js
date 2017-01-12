2222c8de67e9b0555b3732d0e0c53693eb1d9178
Jerry Shanahan <jerry.shanahan@gmail.com>
2017-01-11 19:42:18 -0500

    fixed song dynamic rendering

--- client/reducers/playlistSongs.js
+++ client/reducers/playlistSongs.js
@@ -7,9 +7,11 @@ export default function(state=null, action) {
     console.log("PLAYLIST STATE:", state)
       console.log('FETCH_PLAYLIST_SONGS ACTION: ', action.payload.data);
       let songs = [];
-      action.payload.data.forEach((arr) => {
-        songs.push(arr[0]);
-      })
+      if(Array.isArray(action.payload.data)){
+        action.payload.data.forEach((arr) => {
+          songs.push(arr[0]);
+        })
+      }
       console.log('SONGS!!!!!!: ', songs)
       return songs;
   }
