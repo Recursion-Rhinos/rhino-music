8ee71ce240029687e39b178b25d994610cbf763c
Svetlin Mladenov <svem@gmx.com>
2017-01-11 19:39:34 -0500

    Added new user icon

--- client/containers/Profile/userMenu.js
+++ client/containers/Profile/userMenu.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import toggleUser from '../../actions/toggleUser';
+import Setting from './userSettings';
+import changeSelected  from '../../actions/changeSelected';
 import Paper from 'material-ui/Paper';
 import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
@@ -13,8 +15,8 @@ import ContentCopy from 'material-ui/svg-icons/content/content-copy';
 import Download from 'material-ui/svg-icons/file/file-download';
 import Delete from 'material-ui/svg-icons/action/delete';
 import FontIcon from 'material-ui/FontIcon';
-import Setting from './userSettings';
-import changeSelected  from '../../actions/changeSelected';
+import Verified from 'material-ui/svg-icons/action/verified-user';
+
 
 const style = {
   paper: {
@@ -39,7 +41,7 @@ render () {
       <div>
       <Paper style={style.paper}>
       <Menu>
-        <MenuItem primaryText="Svetlin" leftIcon={<RemoveRedEye />} />
+        <MenuItem primaryText="Svetlin" leftIcon={<Verified />} />
         <MenuItem primaryText="Basic Details" onClick={() => this.props.changeSelected('settings')} leftIcon={<PersonAdd />} />
         <MenuItem primaryText="Playlists" onClick={() => this.props.changeSelected('playlists')} leftIcon={<ContentLink />} />
         <Divider />
