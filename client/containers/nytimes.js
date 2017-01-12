f1ed7a942f8e1190051d1de256f615fa44d48bbc
Svetlin Mladenov <svem@gmx.com>
2017-01-12 14:31:08 -0500

    Added Material UI style to nytimes

--- client/containers/nytimes.js
+++ client/containers/nytimes.js
@@ -4,6 +4,24 @@ import { bindActionCreators} from 'redux';
 import { fetchNews } from '../actions/news_nytimes';
 import NYTimesData from './nytimes';
 
+import {GridList, GridTile} from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import Subheader from 'material-ui/Subheader';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+    overflowY: 'auto',
+  },
+};
+
 class SearchNews extends Component {
   constructor(props) {
     super(props);
