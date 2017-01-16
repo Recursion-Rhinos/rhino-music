e608d512e72baea86c9b4e4ef88c216ed83d294d
Svetlin Mladenov <svem@gmx.com>
2017-01-16 13:54:08 -0500

    ...

--- client/actions/changeSelected.js
+++ client/actions/changeSelected.js
@@ -1,8 +1,8 @@
 import { CHANGE_SELECTED } from '../constants/ActionTypes';
 
-export default function changeSelected (selected) {
-  return {  
+export default function changeSelected(selected) {
+  return { 
     type: 'CHANGE_SELECTED',
     payload: selected
-  }
+  };
 }
--- client/actions/currentUser.js
+++ client/actions/currentUser.js
@@ -2,9 +2,10 @@ import axios from 'axios';
 import {GET_USER} from '../constants/ActionTypes';
 
 export default function getUser() {
-  let currentUser = axios.get('/api/getUserInfo');
-	return {
-	  type: "GET_USER",
-	  payload: currentUser
-	};
+  const currentUser = axios.get('/api/getUserInfo');
+  return {
+    type: 'GET_USER',
+    payload: currentUser
+  };
 }
+
