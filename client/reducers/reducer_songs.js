07978bf0170869021324ab3f5b9a2f33ea67963b
Narmin Shahin <narmin@vimeo.com>
2017-11-15 16:20:49 -0500

    Update reducer_songs.js

--- client/reducers/reducer_songs.js
+++ client/reducers/reducer_songs.js
@@ -3,7 +3,7 @@ import { PLAY_SONGS, GET_NEWS } from '../constants/ActionTypes';
 export default function(state=null, action) {
   switch(action.type) {
   case "PLAY_SONGS":
-    return action.payload //object BEST PRACTISE
+    return action.payload 
   }
   return state;
 }
